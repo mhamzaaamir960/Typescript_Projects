c204b6ac66f6ca8404f4c7ed3edd04919fabec5d
mhamzaaamir960 <mhamzaaamir960.gmail.com>
2023-10-18 23:15:14 +0500

    updated

--- 01_no-guessing-game_project/To Do list/banner.ts
+++
@@ -1,6 +0,0 @@
-import chalk from "chalk"
-import showBanner from "node-banner"
-
-export async function node_banner() {
-    await showBanner("To DO List")
-}
--- 01_no-guessing-game_project/To Do list/view_tasks.ts
+++
@@ -1,8 +0,0 @@
-import { tasks } from "./main.js";
-import chalk from "chalk";
-
-export function view_tasks() {
-    return tasks.forEach((task) => {
-      console.log((`${chalk.grey(`Task ${task.index}:`)} \n${chalk.blue(`Description:`)} ${chalk.green(task.description)}`))
-  })
-}
